56be44b91d9a2bdd08d5dc8e48813cb17febde02
Umair Khan <muhammadumairkhanyz@gmail.com>
2020-02-17 04:21:50 +0500

    .env file added

--- src/server/pg/pg-connection.ts
+++ src/server/pg/pg-connection.ts
@@ -1,11 +1,14 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const pgp = require('pg-promise')();
 
 const cn = {
-    host: 'ec2-34-192-30-15.compute-1.amazonaws.com',
-    port: 5432,
-    database: 'd1j134osajc9pe',
-    user: 'ncidljgeqeyytr',
-    password: '58068161f57eb70bfb75e9a8653aec6d8ff751b242d0fb25b1c4889c44dfc923',   
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER_NAME,
+    password: process.env.DB_PASSWORD,
     ssl: {
         rejectUnauthorized: false,
     }
